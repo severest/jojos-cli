a1c89261cc0988a4a35c82582473a80b543d783b
Jonas Trappenberg <jonas@7geese.com>
2017-06-30 10:57:50 -0700

    Find branch name for new pull request

--- jojos/commands/pr.py
+++ jojos/commands/pr.py
@@ -1,9 +1,12 @@
 """The pr command."""
-
+import re
+from itertools import count
+from pprint import pprint
 from subprocess import check_output
-from json import dumps
 
 from .base import Base
+
+_cookies = None
 
 
 class Pr(Base):
@@ -16,4 +19,20 @@ class Pr(Base):
         print(check_output(['git', 'checkout', 'staging']))
         print(check_output(['git', 'fetch']))
         print('Searching for branches...')
-        branches = check_output(['git', 'branch', '-a'])
+        branches = [line.strip() for line in check_output(['git', 'branch', '-a']).splitlines()]
+        pprint(branches)
+
+        counter = count(start=1)
+        for number in counter:
+            new_branch = 'issues/{}-{}'.format(issue_id, number)
+            if new_branch in branches or 'remotes/origin/{}'.format(new_branch) in branches:
+                continue
+            break
+
+        print(check_output(['git', 'checkout', '-b', new_branch]))
+        print(check_output(['git', 'commit', '-m', 'Connect to issue #{}\n\n[ci skip]'.format(issue_id), '--allow-empty']))
+        print(check_output(['git', 'push', '-u', 'origin', new_branch]))
+        pr_title = raw_input('Enter PR title: ')
+        pr_url = check_output(['hub', 'pull-request', '-m', pr_title]).strip()
+        pr_id = re.search(r'/pull/(\d+)', pr_url).group(1)
+        check_output(['open', pr_url])
