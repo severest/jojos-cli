8f10eeb43d2abb3d48c699ffd7c864ccf85e0f75
Jonas Trappenberg <jonas@7geese.com>
2018-10-20 16:57:06 -0700

    Missed a file

---
+++ jojos/commands/merge.py
@@ -0,0 +1,28 @@
+"""The open command."""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from pprint import pprint
+from subprocess import check_output, call
+
+from ..utils import github
+from .base import Base
+
+
+class Merge(Base):
+    """
+    Squash-merge a PR as GitHub would do it, i.e. by creating a commit with a message like this:
+
+        Make template dropdown animation reusable (#6816)
+
+    """
+
+    def run(self):
+        pr_id = self.options['<pull_request_id>']
+        pr_data = github.get_pr(pr_id)
+        retcode = call(['git', 'merge', '--squash', '--no-edit', '--commit', 'origin/{}'.format(pr_data['head']['ref'])])
+        if retcode != 0:
+            exit(retcode)
+        retcode = call(['git', 'commit', '-m', '{} ({})'.format(pr_data['title'], pr_id)])
+        if retcode != 0:
+            exit(retcode)
+
